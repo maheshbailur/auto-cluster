df718ddf57de103ba32b08b1afe04698f5073e9a
maheshbailur <maheshbailur@gmail.com>
2023-12-29 16:45:24 +0000

    updated bastion

--- kubernetes-cluster/instance.tf
+++ kubernetes-cluster/instance.tf
@@ -6,9 +6,9 @@ locals {
 resource "aws_instance" "bastion_host" {
   ami                    = "ami-0430580de6244e02e"
   instance_type          = "t2.micro"
-  subnet_id              = locals.terra_data.public_subnet_ids.value[0]
-  key_name               = locals.terra_data.key_name.value
-  vpc_security_group_ids = [locals.terra_data.bastion_host_sg.value]
+  subnet_id              = local.terra_data.public_subnet_ids.value[0][0]
+  key_name               = local.terra_data.key_name.value
+  vpc_security_group_ids = [local.terra_data.bastion_host_sg.value]
   tags = {
     Name = "bastion_host"
   }
@@ -16,5 +16,5 @@ resource "aws_instance" "bastion_host" {
 
 
 output "PublicIP" {
-  value = aws_instance.dove-web.public_ip
+  value = aws_instance.bastion_host.public_ip
 }
